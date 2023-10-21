335047b55739e740e6d566f2424597c9fae7b481
Shubh Arya <shubh.arya37@gmail.com>
2023-10-21 11:43:33 +0530

    Pet component styling and adding props

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,11 +1,21 @@
-const Pet = (props) => {
-    return (
-      <div>
-        <h1>{props.name}</h1>
-        <h2>{props.animal}</h2>
-        <h2>{props.breed}</h2>
+const Pet = ({ animal, name, location, breed, image, id }) => {
+  let hero = "http://pets-v2.dev-apis.com/pets/none.jpg";
+  if (image.length) {
+    hero = image[0];
+  }
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
       </div>
-    );
-  };
-  
-  export default Pet;
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
+      </div>
+    </a>
+  );
+};
+
+export default Pet;
