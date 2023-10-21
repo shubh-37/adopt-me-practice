7022e50ef25422e25f40ba821f624203d52e0d63
Shubh Arya <shubh.arya37@gmail.com>
2023-10-21 11:44:40 +0530

    a reusable component to display pets

---
+++ src/PetResults.jsx
@@ -0,0 +1,24 @@
+import Pet from "./Pet";
+
+const PetResults = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No pets found</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            animal={pet.animal}
+            name={pet.name}
+            location={`${pet.city} ${pet.state}`}
+            image={pet.images}
+            breed={pet.breed}
+            key={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default PetResults;
