3457c315b21aad2e3b4631159d80440e9a880872
Shubh Arya <shubh.arya37@gmail.com>
2023-10-22 09:44:33 +0530

    added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-import Pet from "./Pet";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 const container = document.getElementById("root");
